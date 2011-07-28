e74fbc299ef03e9d2ed7bcd7debd08b27cd33fe3
Darren Shafae <dshafae@papercheck.com>
2011-07-27 18:55:16 -0700

    Found a bug in my logic, where if the paddle is over to the far left, it will always bounce, as if the entire bottom of the floor has been raised.

--- simple2.js
+++ simple2.js
@@ -14,8 +14,14 @@ function ball() {
     this.paddel.id = "paddel";
     this.paddel.width = "180";
     this.paddel.height = "20";
-    this.paddel.style.left = "271px";
+    this.paddel.style.left = "300px";
     this.paddel.style.top = "480px";
+
+    this.brick = document.createElement('div');
+    this.brick.className = "brick";
+    this.brick.width = "50";
+    this.brick.height = "20";
+    this.brick.style.left = "300px";
 
     this.container = document.getElementById('container');
     this.container.width = "800";
@@ -23,6 +29,7 @@ function ball() {
     this.container.appendChild(this.element);
     this.container.appendChild(this.audio);
     this.container.appendChild(this.paddel);
+    this.container.appendChild(this.brick);
 
     this.width = function () {
         return parseInt(this.element.width);
@@ -52,17 +59,31 @@ function ball() {
         return parseInt(this.element.style.top);
     };
 
+    this.brickHeight = function (){
+        return parseInt(this.brick.style.top)
+    };
+
+    this.brickLeft = function (){
+        return parseInt(this.brick.style.left)
+    };
+
+    this.brickWidth = function (){
+        return parseInt(this.brick.style.width)
+    };
+
     this.center = {'x' : (this.width() / 2) + this.left(),
                    'y' : (this.height() / 2) + this.top() };
 
     this.paddelCenter = {'x' : (this.paddelWidth) + this.leftPaddel,
                          'y' : (this.paddelHeight)};
 
+    this.brickCenter = {'x' : (this.brickWidth()) + this.brickLeft,
+                        'y' : (this.brickHeight())};
+
     this.directionX = 1;
     this.directionY = 1;
     this.paddelRight = -1;
     this.paddelLeft = 1;
-
     this.changeDirection = -1;
 
     this.draw = function (magnitude) {
@@ -71,12 +92,11 @@ function ball() {
         var paddelLeft = this.leftPaddel();
 
         if (isNaN(currentLeft)) { currentLeft = 0}
-        if (isNaN(paddelLeft)) { paddelLeft = 271}
+        if (isNaN(paddelLeft)) { paddelLeft = 300}
         if (isNaN(currentTop)) { currentTop = 0}
 
         var vectorX = currentLeft + (magnitude * this.directionX);
         var vectorY = currentTop + (magnitude * this.directionY);
-
         var vectorXP = paddelLeft + this.paddelLeft + this.paddelRight;
 
         this.center.x = vectorX + (this.width() / 2);
@@ -93,34 +113,53 @@ function ball() {
 
     this.collisionDetect = function () {
         var containerWidth = parseInt(this.container.width);
+        var realXcenter = this.center.x * -1 + containerWidth;
         var containerWidthP = parseInt(this.container.width);
         var containerHeight = parseInt(this.container.height);
         var halfWidth = (this.width() / 2);
         var paddelWidth = (this.paddelWidth());
+        var brickWidth = (this.brickWidth());
+        var brickHeight = (this.brickHeight());
 
+        //ping audio
         if ((this.directionX * this.directionY) + this.changeDirection == 0) {
             this.changeDirection = this.changeDirection * -1;
             this.audio.play();
         }
 
+        //bounce x-axis
         if (this.center.x + halfWidth >= containerWidth || this.center.x - halfWidth < 0) {
             this.directionX = this.directionX * -1;
         }
 
+        //bounce y-axis from the ceiling
         if (this.center.y - halfWidth < 0) {
             this.directionY = this.directionY * -1;
         }
 
+        //bounce y-axis the floor, which is a fail
         if (this.center.y + halfWidth >= containerHeight) {
             this.directionX = 0;
             this.directionY = 0;
         }
 
-        if(containerHeight - this.center.y - halfWidth == this.paddelCenter.y && this.center.x + paddelWidth >= paddelWidth + this.paddelCenter.x) {
+        //bounce off the paddel
+        if(containerHeight - this.center.y - halfWidth == this.paddelCenter.y && realXcenter + paddelWidth <= paddelWidth + this.paddelCenter.x) {
             this.directionY = this.directionY * -1;
+            console.log("side 1: "+(realXcenter));
+            console.log("side 2: "+(this.paddelCenter.x));
         }
+//        else {
+//            console.log("side 1: "+(this.center.x));
+//            console.log("side 2: "+(this.paddelCenter.x));
+//        }
 
+//        //bounce off the bricks
+//        if(containerHeight - brickHeight - this.center.y - halfWidth == this.brickCenter.y && this.center.x + brickWidth >= brickWidth + this.brickCenter.x) {
+//            console.log("off bricks");
+//        }
 
+        //contains the paddel inside the container
         if (this.paddelCenter.x + paddelWidth + 1 >= containerWidthP || this.paddelCenter.x - 1 < 0) {
             this.paddelRight = 0;
             this.paddelLeft = 0;
